06a5ad6439c02219fc428775ebb1dddc20fd0f50
SRISHTI SINGH <143429418+srishtiiyyy@users.noreply.github.com>
2025-10-27 14:29:55 +0000

    DATATYPES

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"//treat all JS CODE AS NEWER VERSION
+
+// alert(3 +3) // WE ARE USING NODE JS AND NOT BROWSER
+
+console.log(3+3)
+console.log("Srishti")
+//CODE READABILITY MUST BE HIGH
+
+let name="srishti"
+let age=18
+let isloggedin=false
+let state;//undefined
+let s=null;//null
+//datatypes
+/*
+number=>2 to power 53
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value
+undefined=>abhi value assign nhi hui hai
+symbol=>unique
+*/
+
+//object
+console.log(typeof("srishti"))
+console.log(typeof (null))//->OBJECT
+console.log(typeof undefined)//->UNDEFINED
