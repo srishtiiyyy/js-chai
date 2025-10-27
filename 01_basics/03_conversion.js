7af922920782d964d2b33d6a59284cf024ee35c9
SRISHTI SINGH <143429418+srishtiiyyy@users.noreply.github.com>
2025-10-27 16:44:37 +0000

    OPERATIONS

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -24,3 +24,36 @@ console.log(booleanisloggedin)
 let somenumber=33
 let stringnumber= String(somenumber)
 console.log(typeof stringnumber)
+// ************************OPERATIONS****************************//
+ let value=3
+ let negvalue=-value
+ console.log(negvalue)
+
+ console.log(2+3)
+ console.log(2-3)
+ console.log(2*3)
+ console.log(2**3)//power
+ console.log(2/3)
+ console.log(2%3)
+
+ let str1="srishti"
+ let str2="singh"
+ let str3=str1+str2
+ console.log(str3)
+
+ console.log("1"+2)//12
+ console.log(1+"2")//12
+
+ console.log("1"+2+2)//122
+
+ console.log(1+2+"2")//32
+ 
+ console.log(+true);//1
+//  console.log(true+)//error
+
+// let num1,num2,num3
+// num1=num2=num3=2+2-->not preferred
+
+let gamecounter=11
+gamecounter++;//++gamecounter
+console.log(gamecounter);//12
